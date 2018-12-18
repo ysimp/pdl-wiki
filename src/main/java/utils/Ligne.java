d5a47714bd46b3ad5b1cdd1b1aa7e30192a8056a
Hermann Egny <hermann.egny@etudiant.univ-rennes1.fr>
2018-12-18 19:12:55 +0100

    Modif Ligne & Cellule

---
+++ src/main/java/utils/Ligne.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.util.List;
+
+public class Ligne {
+	private int numeroLigne;
+	private List<String> ligneTableau;
+	private List<Cellule> listCelluleLigne;
+	
+	public Ligne() {}
+	
+	public Ligne(List<String> ligneTableau) {
+		super();
+		this.ligneTableau = ligneTableau;
+	}
+	
+	
+   
+
+	public List<Cellule> getListCelluleLigne() {
+		return listCelluleLigne;
+	}
+
+	public void setListCelluleLigne(List<Cellule> listCelluleLigne) {
+		this.listCelluleLigne = listCelluleLigne;
+	}
+
+	public int getNumeroLigne() {
+		return numeroLigne;
+	}
+
+	public void setNumeroLigne(int numeroLigne) {
+		this.numeroLigne = numeroLigne;
+	}
+
+	public List<String> getLigneTableau() {
+		return ligneTableau;
+	}
+
+	public void setLigneTableau(List<String> ligneTableau) {
+		this.ligneTableau = ligneTableau;
+	}
+	
+	
+
+}
