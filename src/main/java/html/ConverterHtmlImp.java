cd7d28f3b58a892b469c40b4fc75aa2bda4df57b
jahouiHajar <jahoui.hajar@gmail.com>
2019-11-11 14:16:43 +0100

    Implementation Convertor

---
+++ src/main/java/html/ConverterHtmlImp.java
@@ -0,0 +1,57 @@
+package html;
+
+
+import java.io.FileWriter;
+import java.util.List;
+
+import model.Page;
+import model.Tableau;
+import utils.CSVUtils;
+import utils.Constant;
+
+public class ConverterHtmlImp implements ConverterHtml{
+	private ExtractorHtml extractorHtml;
+	
+	public ConverterHtmlImp() {
+		this.extractorHtml = new ExtractorHtmlImp();
+	}
+
+
+
+	public void convertAllTablesToCsv(String url) throws Exception {
+		String tableauCSV;
+		String fileName;
+		fileName = CSVUtils.constructFileName(url);	
+		FileWriter out=null;
+		
+		Page page = extractorHtml.extractTables(url,false);
+		page.setNomPage(fileName);
+		
+	
+			
+			for (Tableau tab : page.getListeTableau()) {
+				tableauCSV =Constant.CSV_HTML_PATH + CSVUtils.mkCSVFileName(fileName, tab.getNumeroTableau());
+				out = new FileWriter(tableauCSV,false);
+				CSVUtils.writeTable(out, tab);
+					out.flush();
+					out.close();
+			}	
+		
+	
+		
+		
+	}
+
+	public void convertAllPages() throws Exception {
+		
+		List<String> liste = CSVUtils.getListFromFile(Constant.WIKI_URL_PATH);
+		for (String urL : liste) {
+			
+			convertAllTablesToCsv(urL);
+		}
+			
+		
+	}
+
+}
+
